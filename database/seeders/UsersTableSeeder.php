a49cc2463996ceb25949302782fbe8398c1cd800
kuhcretsen1 <148473755+kuhcretsen1@users.noreply.github.com>
2024-06-04 09:38:10 +0300

    feat: Creation of migration and filling of data in tables
    fix

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'name' => 'Невідомий автор',
+                'email' => 'author_unknown@g.g',
+                'password' => bcrypt(Str::random(16)),
+            ],
+            [
+                'name' => 'Автор',
+                'email' => 'author1@g.g',
+                'password' => bcrypt('123123'),
+            ],
+        ];
+        DB::table('users')->insert($data);
+    }
+}
