905df921bf7b77c0ff4c4740f1f7c04520aedcb0
kuhcretsen1 <148473755+kuhcretsen1@users.noreply.github.com>
2024-06-14 15:08:29 +0300

    feat:Observer Transferring Logic from Controllers

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -44,9 +44,7 @@ class CategoryController extends BaseController
     public function store(BlogCategoryCreateRequest $request)
     {
         $data = $request->input(); //отримаємо масив даних, які надійшли з форми
-        if (empty($data['slug'])) { //якщо псевдонім порожній
-            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
-        }
+      
 
         $item = (new BlogCategory())->create($data); //створюємо об'єкт і додаємо в БД
 
